a6888d0f93405cdd87264ac6f5261eba636be283
aturrr62 <522210134@univpancasila.ac.id>
2026-05-04 00:15:58 +0700

    testing PBI 01 dan 02

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -11,6 +11,8 @@ use PHPUnit\Framework\Attributes\BeforeClass;
 
 abstract class DuskTestCase extends BaseTestCase
 {
+    use CreatesApplication;
+
     /**
      * Prepare for Dusk test execution.
      */
